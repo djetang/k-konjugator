259104cda5479db49e3579d488bd48890d21e326
djetang <79625214+djetang@users.noreply.github.com>
2021-03-28 18:32:17 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const { kKonjugator } = require ("./scripts/konjugator");
+
+const client = new Discord.Client({
+    partials: ['Message']
+});
+
+client.on('ready',() => {
+    console.log('\nBot is online!\n');
+});
+
+client.on('message', (message) => {
+    const text = message.content;
+    const textArray = text.split(' ');
+    if (message.author.id === process.env.CLIENT_ID);
+    else if (message.channel.id !== process.env.BOT_ACTIVE_CHANNEL) {
+        message.reply('plz type this in the correct channel!');
+    }
+    else if ((text.includes(`<@!${process
+        .env.CLIENT_ID}>`) && text.includes("konjugate")) || text.includes ("!k")) {
+        if (textArray.length === 4) 
+            message.reply(kKonjugator(textArray[1], textArray[2], textArray[3]));
+        else
+            message.reply('If you want to konjugate, type: !k verb level tense');
+    };
+});
+
+client.login(process.env.ACCESS_TOKEN);
---
+++ src/scripts/konjugator.js
@@ -0,0 +1,275 @@
+//Welcome to the conjugator! 
+
+const Hangul = require('hangul-js');
+const validTenses = ['past', 'present', 'future'];
+const validLevels = ['informalLow', 'informalHigh', 'formalHigh'];
+const validVowels = ['ㅏ','ㅑ','ㅓ','ㅕ','ㅗ','ㅛ','ㅜ','ㅠ','ㅡ','ㅣ','ㅐ','ㅒ','ㅔ','ㅖ']
+const irregularVerbEnd = ['ㅅ', 'ㄷ', 'ㅂ', 'ㅡ', 'ㄹ']
+const irregularVerbWord = ['르']
+const bieutExceptionsOh = ['돕다', '곱다']
+const irregularExceptions = ['웃다','벗다','씻다','걷다','받다','묻다','닫다','좁다','잡다','넓다','따르다']
+
+/**
+ * 
+ *@returns {string[]} 
+ */
+function showMeTheBlocks(word) {
+    return word.split('')
+} //should return an array of word blocks
+
+function isThisSomething(word, whatMightItBe) {
+    if (word.length < whatMightItBe.length)
+        return false;
+    else 
+        return word.lastIndexOf(whatMightItBe) === word.length - whatMightItBe.length;
+}
+
+function isThisDah(word) {
+    return isThisSomething(word, '다');
+}
+
+function isThisHaDah(word){
+    return isThisSomething(word, '하다');
+}
+
+function isThisSibNiDah(word){
+    return isThisSomething(word, '습니다');
+}
+
+function isThisIbNiDah(word){
+    return isThisSomething(word, '입니다');
+}
+
+function identifyLastStem(word) {
+    const cutdah = cutTheLastDah(word)
+    const cuthadah = cutTheLastHaDah(word)
+    if (isThisHaDah(word))
+        return (cuthadah.substring(cuthadah.length-1, cuthadah.length)); /*this specific one is the same as identifyFirstStem */
+    else if (isThisDah(word))
+        return (cutdah.substring(cutdah.length-1, cutdah.length));
+    else return ('this aint a verb again')
+} //finds the last block before 다/하다
+
+function lastStemLetters(word) {
+    const lastStem = identifyLastStem(word)
+    return Hangul.disassemble(lastStem)
+} //should return an array of the last hangul letter, split up
+
+function veryLastLetter(word){
+    const stemletters = lastStemLetters(word)
+    return stemletters[stemletters.length-1];
+} //returns only the very last Hangul letter of the stem
+
+function isThisWah(word) {
+    const letters = lastStemLetters(word)
+    return ( (letters[1] === 'ㅗ' && letters[2] !== 'ㅐ') &&
+                (letters[1] === 'ㅗ' && letters[2] !== 'ㅣ')) ||
+        letters[1] === 'ㅏ' ;
+}
+
+function cutTheLastDah(word) {
+    const allBlocks = showMeTheBlocks(word)
+    return (word.substring(0,(allBlocks.length-1)))
+} //shows just the word stem without 다
+
+function cutTheLastDahLetters(word) {
+    const blocks = cutTheLastDah(word)
+    return Hangul.disassemble(blocks);
+} //shows just the word stem without 다 as an array of letters
+
+function cutTheLastHaDah(word) {
+    const allBlocks = showMeTheBlocks(word)
+    return (word.substring(0,(allBlocks.length-2)));
+} //shows just the word stem without 하다
+
+function cutTheLastHaDahLetters(word) {
+    const blocks = cutTheLastHaDah(word)
+    return Hangul.disassemble(blocks);
+} //shows just the word stem without 하다 as an array of letters
+
+function handleFormalHighPresent(cut, veryLL, level, tense){
+    if (validVowels.includes(veryLL))
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['아'].slice(2)]); 
+    else
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['아']]); 
+}
+
+function handleAllWah(cut, veryLL, level, tense){
+    if (veryLL == 'ㅗ' && tense !== 'future')
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['아'].slice(1)]); /** ㅗ:  delete ㅇ,  assemble */
+    else if (veryLL == 'ㅏ' && tense !== 'future')
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['아'].slice(2)]); /** ㅏ: delete ㅇ, ㅏ, assemble */
+    else
+        return Hangul.assemble ([...cut, 'ㄹ', ...(conjugations[level][tense]['아'])]);
+}
+
+function handleSiotAndDigeut(cut, veryLL, word, level, tense){
+    const stemletters = lastStemLetters(word)
+    const oneshortLL = stemletters[stemletters.length-2]
+    const oneshortcut = (cut.slice(0,cut.length-1))
+    if (veryLL == 'ㅅ' && tense === 'past')
+        return handleAsRegulars(oneshortcut, oneshortLL, word, level, tense);
+    else if (veryLL == 'ㅅ' && tense === 'present' && (level === 'informalLow' || level === 'informalHigh'))
+        return handleAsRegulars(oneshortcut, oneshortLL, word, level, tense);
+    else 
+        return handleAsRegulars(cut, veryLL, word, level, tense);
+}
+
+function handleBieup(cut, veryLL, word, level, tense){
+    const newOCut = ([...cut.slice(0, cut.length-1), 'ㅇ', 'ㅗ'])
+    const newUCut = ([...cut.slice(0, cut.length-1), 'ㅇ', 'ㅜ'])
+    const newOLL = 'ㅗ'
+    const newULL = 'ㅜ'
+    if (bieutExceptionsOh.includes(word))
+        if (tense === 'past')
+            return handleAsRegulars(newOCut, newOLL, word, level, tense);
+        else if (tense === 'present' && (level === 'informalLow' || level === 'informalHigh'))
+            return handleAsRegulars(newOCut, newOLL, word, level, tense);
+    else 
+        if (tense === 'past')
+            return handleAsRegulars(newUCut, newULL, word, level, tense);
+        else if (tense === 'present' && (level === 'informalLow' || level === 'informalHigh'))
+            return handleAsRegulars(newUCut, newULL, word, level, tense); 
+        else 
+            return handleAsRegulars(cut, veryLL, word, level, tense);
+}
+
+// function handleEu(cut, veryLL, word, level, tense){
+//     const oneshortcut = (cut.slice(0,cut.length-1))
+//     const decidingLL = findTheVowel(cut)
+//     if (tense === 'past')
+//         return handleAsRegulars(oneshortcut, decidingLL, word, level, tense);
+//     else if (tense === 'present' && (level === 'informalLow' || level === 'informalHigh'))
+//         return handleAsRegulars(oneshortcut, decidingLL, word, level, tense); 
+//     else 
+//         return handleAsRegulars(cut, veryLL, word, level, tense);
+// }
+
+// function findTheVowel(cut){
+//     if (cut.length < 3)
+//         return 'ㅓ';
+//     else if ((cut.slice(cut.length-4,cut.length-3)).includes(validVowels))
+//         return ([...cut.slice(cut.length-4,cut.length-3)]);
+//     else if ((cut.slice(cut.length-3,cut.length-2)).includes(validVowels))
+//         return ([...cut.slice(cut.length-3,cut.length-2)]);
+//     else
+//         return 'something wrong'
+// }
+
+function handleAllIrregulars(cut, veryLL, word, level, tense){
+    if (veryLL == 'ㅅ' || veryLL == 'ㄷ')
+        return handleSiotAndDigeut(cut, veryLL, word, level, tense);
+    else if (veryLL == 'ㅂ')
+        return handleBieup(cut, veryLL, word, level, tense);
+   // else if (veryLL == 'ㅡ')
+   //     return handleEu(cut, veryLL, word, level, tense);
+    else
+        return 'not done';
+}
+
+function handleAsRegulars(cut, veryLL, word, level, tense){
+    if (level == 'formalHigh' && tense == 'present') /** for 습니다 / ~ㅡㅂ 니다 */
+        return handleFormalHighPresent(cut, veryLL, level, tense);
+    else if ((veryLL == ('ㅜ' || 'ㅡ')) && tense == ('past' || 'present'))
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['어'].slice(1)]); /** ㅜ or ㅡ: delete ㅇ, assemble */
+    else if ((veryLL == ('ㅓ' || 'ㅕ')) && tense == ('past' || 'present'))
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['어'].slice(2)]); /** ㅓ or ㅕ: delete ㅇ, ㅓ, assemble */
+    else if (veryLL == 'ㅣ' && tense == ('past' || 'present'))
+        return Hangul.assemble ([...cut.slice(0,cut.length-1), 'ㅕ', ...conjugations[level][tense]['어'].slice(2)]); /**ㅣ: delete ㅣ,ㅇ,ㅓ, add 여, assemble */
+    else if (isThisWah(word))
+        return handleAllWah(cut, veryLL, level, tense);
+    else if (validVowels.includes(veryLL) && tense == ('future')) 
+        return Hangul.assemble ([...cut, 'ㄹ', ...conjugations[level][tense]['어']]); /** future tense with vowel ending */
+    else if (tense == 'future')
+        return Hangul.assemble ([...cut, 'ㅇ', 'ㅡ', 'ㄹ', ...conjugations[level][tense]['어']]); /** future tense with consonant ending */
+    else 
+        return Hangul.assemble ([...cut, ...conjugations[level][tense]['어']]); /** all others without vowel ending */
+}
+
+function conjugateDahYo(word,level,tense) {
+    const cut = cutTheLastDahLetters(word)
+    const veryLL = veryLastLetter(word)
+    if (irregularExceptions.includes(word))
+        return handleAsRegulars(cut, veryLL, word, level, tense);
+    else if (irregularVerbEnd.includes(veryLL))
+        return handleAllIrregulars(cut, veryLL, word, level, tense);
+    else
+        return handleAsRegulars(cut, veryLL, word, level, tense);
+}
+
+function conjugateHaDahYo(word,level,tense) {
+    const cut = cutTheLastHaDahLetters(word)
+    return Hangul.assemble ([...cut, ...conjugations[level][tense]['하']]);
+    }
+
+function kKonjugator (word,level,tense){
+    if (!validLevels.includes(level) || (!validTenses.includes(tense)) )
+        return 'include a proper tense!';
+    else 
+        if (isThisHaDah(word)) 
+            return conjugateHaDahYo(word,level,tense);
+        else if (isThisSibNiDah(word))
+            return 'Give me the dictionary form, this is the formal form!';
+        else if (isThisIbNiDah(word))
+            return 'this is the formal form of TO BE';
+        else if (isThisDah(word))
+            return conjugateDahYo(word,level,tense);
+        else  
+            return 'this doesnt look like a dictionary form verb to me';
+}
+
+let conjugations = {
+    informalLow: {
+        past:{
+            아: ['ㅇ','ㅏ','ㅆ','ㅇ','ㅓ'], /* ㅗ merge, ㅏ collapse */
+            어: ['ㅇ','ㅓ','ㅆ','ㅇ','ㅓ'], /*  */
+            하: ['ㅎ','ㅐ','ㅆ','ㅇ','ㅓ'],
+        },
+        present:{
+            아: ['ㅇ','ㅏ'],
+            어: ['ㅇ','ㅓ'],
+            하: ['ㅎ','ㅐ'],
+        },
+        future:{
+            아: ['ㄱ','ㅔ','ㅆ','ㅇ','ㅓ'],
+            어: ['ㄱ','ㅔ','ㅆ','ㅇ','ㅓ'],
+            하: ['ㅎ','ㅏ','ㄹ','ㄱ','ㅓ','ㅇ','ㅑ'],
+        },
+    },
+    informalHigh: {
+        past:{
+            아: ['ㅇ','ㅏ','ㅆ','ㅇ','ㅓ','ㅇ','ㅛ'],
+            어: ['ㅇ','ㅓ','ㅆ','ㅇ','ㅓ','ㅇ','ㅛ'],
+            하: ['ㅎ','ㅐ','ㅆ','ㅇ','ㅓ','ㅇ','ㅛ'],
+        },
+        present:{
+            아: ['ㅇ','ㅏ','ㅇ','ㅛ'],
+            어: ['ㅇ','ㅓ','ㅇ','ㅛ'],
+            하: ['ㅎ','ㅐ','ㅇ','ㅛ'],
+        },
+        future:{
+            아: ['ㄱ','ㅔ','ㅆ','ㅇ','ㅓ','ㅇ','ㅛ'],
+            어: ['ㄱ','ㅔ','ㅆ','ㅇ','ㅓ','ㅇ','ㅛ'],
+            하: ['ㅎ','ㅏ','ㄹ','ㄱ','ㅓ','ㅇ','ㅖ','ㅇ','ㅛ'],
+        },
+    },
+    formalHigh: {
+        past:{
+            아: ['ㅇ','ㅏ','ㅆ','ㅅ','ㅡ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+            어: ['ㅇ','ㅓ','ㅆ','ㅅ','ㅡ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+            하: ['ㅎ','ㅐ','ㅆ','ㅅ','ㅡ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+        },
+        present:{
+            아: ['ㅅ','ㅡ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'], 
+            어: ['ㅅ','ㅡ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+            하: ['ㅎ','ㅏ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+        },
+        future:{
+            아: ['ㄱ','ㅓ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'], 
+            어: ['ㄱ','ㅓ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+            하: ['ㅎ','ㅏ','ㄹ',' ','ㄱ','ㅓ','ㅅ','ㅇ','ㅣ','ㅂ','ㄴ','ㅣ','ㄷ','ㅏ'],
+        },
+    }
+};
+
+module.exports = { kKonjugator };
