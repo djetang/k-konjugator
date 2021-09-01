33b22cbf774b955dca55d20e54770d925dbe0bb3
djetang <79625214+djetang@users.noreply.github.com>
2021-08-31 22:20:40 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const Discord = require('discord.js');
+const {Client, Intents} = require('discord.js');
 require('dotenv').config();
 
 const { kKonjugator } = require ("./scripts/konjugator");
 
-const client = new Discord.Client({
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+    ],
     partials: ['Message']
 });
 
@@ -11,17 +19,17 @@ client.on('ready',() => {
     console.log('\nBot is online!\n');
 });
 
-client.on('message', (message) => {
+client.on('messageCreate', (message) => {
     const text = message.content;
     const textArray = text.split(' ');
     if (message.author.id === process.env.CLIENT_ID);
     else if (text.includes ("!k")) {
         if (message.channel.id !== process.env.BOT_ACTIVE_CHANNEL)
-            message.reply('plz type this in the correct channel!');
+            message.channel.send(`${message.author},plz type this in the correct channel!`);
         else if (textArray.length === 4) 
             message.reply(kKonjugator(textArray[1], textArray[2], textArray[3]));
         else
-            message.reply('If you want to konjugate, type: !k verb level tense');
+            message.channel.send(`${message.author},If you want to konjugate, type: !k verb level tense`);
     };
 });
 
